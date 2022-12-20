a9599f9586fce90d1452b86a43aaa700595545f2
Hithesh21 <hithesh.xworkz@gmail.com>
2022-12-20 19:40:35 +0530

    collection methods

---
+++ cllection/src/com/xworkz/cllection/boot/collectRunner.java
@@ -0,0 +1,209 @@
+package com.xworkz.cllection.boot;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class collectRunner {
+
+	public static void main(String[] args) {
+
+		
+		Collection<String> collect=new ArrayList();
+		collect.add("phenix");
+		collect.add("lido");
+		collect.add("forum");
+		collect.add("nexus");
+		collect.add("orion");
+		collect.add("gt");
+		collect.add("lulu");
+		collect.add("star");
+		collect.add("mantri");
+		collect.add("garuda");
+		collect.add("inox");
+		collect.add("pvr");
+		collect.add("rr");
+
+        System.out.println(collect);
+
+         int total=collect.size();
+
+        		 System.out.println(total);
+        		 
+        		 System.out.println("after clear...");
+        		 
+        		collect.clear();
+        		
+        		System.out.println("===========curency===================");
+        		Collection<String> collect1=new LinkedList();
+        		collect1.add("rupee");
+        		collect1.add("rupayya");
+        		collect1.add("euro");
+        		collect1.add("dollar");
+        		collect1.add("yen");
+        		collect1.add("bdt");
+        		collect1.add("bgn");
+        		collect1.add("bbd");
+        		collect1.add("bhd");
+        		collect1.add("pound");
+        		collect1.add("lek");
+        		collect1.add("lira");
+        		collect1.add("krone");
+        		collect1.add("peso");
+        		collect1.add("shekel");
+        		collect1.add("leu");
+        		collect1.add("rial");
+        		collect1.add("ruble");
+        		collect1.add("reel");
+        		collect1.add("reil");
+        		collect1.add("dinar");
+        		collect1.add("nepal rupee");
+        		collect1.add("baht");
+        		collect1.add("chili peso");
+        		collect1.add("thai bahat");
+        		collect1.add("naira");
+        		collect1.add("zolty");
+        		collect1.add("won");
+        		collect1.add("ringgit");
+        		collect1.add("reel");
+        		collect1.add("swiss");
+        		collect1.add("pound sterling");
+        		
+        		 System.out.println(collect1);
+
+                 int total1=collect1.size();
+
+                		 System.out.println(total1);
+                		 
+                		 System.out.println("after clear...");
+                		 
+                		collect.clear();
+                		
+                		System.out.println("===========coupon===================");
+                		Collection<String> collect2=new HashSet();
+                		collect2.add("paytm");
+                		collect2.add("amazon");
+                		collect2.add("phonepay");
+                		collect2.add("paypal");
+                		collect2.add("flipkart");
+                		collect2.add("shopsy");
+                		collect2.add("myntra");
+                		collect2.add("ajio");
+                		collect2.add("big bazzar");
+                		collect2.add("reliance smart");
+                		collect2.add("dmart");
+                		collect2.add("bigbasket");
+                		collect2.add("tata");
+                		collect2.add("bajapay");
+                		collect2.add("star");
+                		collect2.add("more");
+                		collect2.add("west side");
+                		collect2.add("reebok");
+                		collect2.add("nike");
+                		collect2.add("amazonpay");
+                		
+                		System.out.println(collect2);
+
+                        int total2=collect1.size();
+
+                       		 System.out.println(total2);
+                       		 
+                       		 System.out.println("after clear...");
+                       		 
+                       		collect.clear();
+                		
+                		
+                		
+                		
+                		
+                       		System.out.println("===========sweet===================");
+                    		Collection<String> collect3=new LinkedHashSet();
+                		
+                		
+                		
+                		
+                		collect3.add("jamoon");
+                		collect3.add("ladoo");
+                		collect3.add("jilebi");
+                		collect3.add("kalakand");
+                		collect3.add("champakali");
+                		collect3.add("chum chum");
+                		collect3.add("pheni");
+                		collect3.add("payasa");
+                		collect3.add("jahangir");
+                		collect3.add("badusha");
+                		collect3.add("burfi");
+                		collect3.add("sweet1");
+                		collect3.add("sweet2");
+                		collect3.add("sweet3");
+                		collect3.add("sweet4");
+                		collect3.add("sweet5");
+                		collect3.add("sweet6");
+                		collect3.add("sweet8");
+                		collect3.add("sweet9");
+                		collect3.add("sweet10");
+                		
+                		
+                		
+                		
+                		
+                		
+                		
+                		System.out.println(collect3);
+
+                        int total3=collect1.size();
+
+                       		 System.out.println(total3);
+                       		 
+                       		 System.out.println("after clear...");
+                       		 
+                       		collect.clear();
+
+
+
+                       		System.out.println("===========bike===================");
+                    		Collection<String> collect4=new TreeSet();
+                    		
+                    		collect4.add("bajaj");
+                    		collect4.add("hero");
+                    		collect4.add("activa");
+                    		collect4.add("tvs");
+                    		collect4.add("star city");
+                    		collect4.add("punch");
+                    		collect4.add("platina");
+                    		collect4.add("mahindra");
+                    		collect4.add("ola bike");
+                    		collect4.add("apche");
+                    		collect4.add("bullet");
+                    		collect4.add("splendor");
+                    		collect4.add("ct100");
+                    		collect4.add("suzuki");
+                    		collect4.add("acess");
+                    		collect4.add("bounce");
+                    		collect4.add("pulsar");
+                    		collect4.add("bike1");
+                    		collect4.add("bike2");
+                    		collect4.add("bike3");
+                    		
+                    		
+
+                    		System.out.println(collect4);
+
+                            int total4=collect1.size();
+
+                           		 System.out.println(total4);
+                           		 
+                           		 System.out.println("after clear...");
+                           		 
+                           		collect.clear();
+
+
+
+		
+	}
+
+}
+
